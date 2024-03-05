5c6be1616ca8865d02cf5bf34816dd5b388d135f
Michael Mattingly <mikeamatt1@hotmail.com>
2024-03-04 20:34:14 -0500

    Updated CSS and Homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import './pageStyles/HomeStyles.css';
+import logo from '../assets/clam_lords_logo.png';
 
 function Home() {
   return (
     <div className="home-body">
+      <img src={logo} alt="Clam Lords Logo" />
+      <h1>Welcome to the Clam Lords' Home Page!</h1>
     </div>
   );
 }
--- src/pages/Logs.jsx
+++ src/pages/Logs.jsx
@@ -3,32 +3,34 @@ import "./pageStyles/LogsStyles.css";
 function Logs() {
   return (
     <div className="logs-container">
-      <h2>Welcome to the Clam Lords' World of Logs Page!</h2>
-      <p>
-        Embark on an epic journey through our conquests in the realms of World
-        of Warcraft.
-      </p>
-      <div>
-        <a
-          href="https://sod.warcraftlogs.com/guild/us/chaos-bolt/clam%20lords"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src="https://assets.rpglogs.com/img/warcraft/favicon.png?v=2"
-            alt="Warcraft Logs"
-          />
-        </a>
-        <p>Explore Our Logs</p>
+      <div className="logs-content">
+        <h2>Welcome to the Clam Lords' World of Logs Page!</h2>
+        <p>
+          Embark on an epic journey through our conquests in the realms of World
+          of Warcraft.
+        </p>
+        <div>
+          <a
+            href="https://sod.warcraftlogs.com/guild/us/chaos-bolt/clam%20lords"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://assets.rpglogs.com/img/warcraft/favicon.png?v=2"
+              alt="Warcraft Logs"
+            />
+          </a>
+          <p>Click the icon to open the World of Logs Page</p>
+        </div>
+        <p className="log-title">
+          Dive into the heart of our adventures and explore the intricate logs
+          of our conquests across the realms of World of Warcraft. Witness our
+          triumphs, unravel our strategies, and delve into the depths of our
+          encounters. Embark on a journey through our legendary logs, where
+          every click unveils tales of valor, camaraderie, and epic loot. Join
+          us in reliving the moments that define our Clan's legacy.
+        </p>
       </div>
-      <p className="log-title">
-        Dive into the heart of our adventures and explore the intricate logs of
-        our conquests across the realms of World of Warcraft. Witness our
-        triumphs, unravel our strategies, and delve into the depths of our
-        encounters. Embark on a journey through our legendary logs, where every
-        click unveils tales of valor, camaraderie, and epic loot. Join us in
-        reliving the moments that define our Clan's legacy.
-      </p>
     </div>
   );
 }
