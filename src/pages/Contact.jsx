79925d2beea3c4f516819b24c3f8a8b12a064c74
Michael Mattingly <mikeamatt1@hotmail.com>
2024-03-04 22:27:58 -0500

    updated for process.env

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,6 @@
 import "./pageStyles/ContactStyles.css";
 import { useRef } from "react";
 import emailjs from "@emailjs/browser";
-const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
-const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
-const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
 export const Contact = () => {
   const form = useRef();
@@ -13,11 +10,11 @@ export const Contact = () => {
 
     emailjs
       .sendForm(
-        SERVICE_ID || process.env.EMAILJS_SERVICE_ID,
-        TEMPLATE_ID || process.env.EMAILJS_TEMPLATE_ID,
+        process.env.EMAILJS_SERVICE_ID,
+        process.env.EMAILJS_TEMPLATE_ID,
         form.current,
         {
-          publicKey: PUBLIC_KEY || process.env.EMAILJS_PUBLIC_KEY,
+          publicKey: process.env.EMAILJS_PUBLIC_KEY,
         }
       )
       .then(
